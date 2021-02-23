5ee95b8e30976bb2c80c7ad7ff91d936d8dcf3af
absurpone <absurppone@gmail.com>
2021-02-23 19:18:22 +0900

    ユーザー新規登録機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-    
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX  
+  
   validates :nickname, presence: true
   validates :family_name, presence: true
   validates :first_name, presence: true
