63820d2728da9a0bcd80aa5e1af098ada3edace8
absurpone <absurppone@gmail.com>
2021-02-24 18:20:44 +0900

    ユーザーモデルの単体テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,6 @@ class User < ApplicationRecord
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX  
-  
   validates :nickname, presence: true
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ } do
     validates :family_name
