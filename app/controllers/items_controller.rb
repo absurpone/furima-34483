ca0e9d49efc20f8bfbd96868be3b34558a827956
absurpone <absurppone@gmail.com>
2021-02-26 14:56:41 +0900

    投稿機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,8 +3,21 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :price, :title, :detail, :category_id, :condition_id, :pays_postage_id, :prefecture_id, :shipping_date_id).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,21 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :pays_postage
+  belongs_to :prefecture
+  belongs_to :shipping_date
+  
   belongs_to :user
   has_one_attached :image
 
   validates :price, presence: true # 価格のバリデーション、半角数字で入力の設定
   validates :title, presence: true
   validates :detail, presence: true
-  validates :category_id, presence: true
-  validates :condition_id, presence: true
-  validates :pays_postage_id, presence: true
-  validates :prefecture_id, presence: true
-  validates :shipping_date_id, presence: true
+  validates :category_id, presence: true, numericality: { other_than: 1 }
+  validates :condition_id, presence: true, numericality: { other_than: 1 }
+  validates :pays_postage_id, presence: true, numericality: { other_than: 1 }
+  validates :prefecture_id, presence: true, numericality: { other_than: 1 }
+  validates :shipping_date_id, presence: true, numericality: { other_than: 1 }
   validates :image, presence: true
 end
