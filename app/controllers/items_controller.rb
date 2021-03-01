8008118841985d27862c46cdde1161ac696786c0
absurpone <absurppone@gmail.com>
2021-03-01 17:50:38 +0900

    編集機能の実装 / 出品者のみ編集画面へ遷移可の設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, except: [:index, :new, :create]
+  before_action :contributor_confirmation, only: [:edit]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -19,13 +21,17 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
   end
 
   def update
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
+    else
+      render :edit
+    end
   end
 
   private
@@ -33,5 +39,17 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :price, :title, :detail, :category_id, :condition_id, :pays_postage_id, :prefecture_id,
                                  :shipping_date_id).merge(user_id: current_user.id)
+  end
+end
+
+private
+
+def set_item
+  @item = Item.find(params[:id])
+end
+
+def contributor_confirmation
+  unless current_user == @item.user
+    redirect_to root_path
   end
 end
