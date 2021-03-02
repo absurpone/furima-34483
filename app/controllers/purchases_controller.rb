5bd747190c20712c499d4c977b22fb0bd2a7cb3a
absurpone <absurppone@gmail.com>
2021-03-02 20:00:51 +0900

    購入画面表示の設定

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,4 +1,10 @@
 class PurchasesController < ApplicationController
   def index
+    @item = Item.find(params[:item_id])
+    @purchase_address = PurchaseAddress.new
   end
+
+  def create
+  end
+
 end
