6882e05b53b45943f6997e8fe32316f7df9321bd
absurpone <absurppone@gmail.com>
2021-03-03 18:26:17 +0900

    未ログインユーザーは購入画面へ遷移できない設定

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,4 +1,6 @@
 class PurchasesController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :create]
+  
   def index
     @item = Item.find(params[:item_id])
     @purchase_address = PurchaseAddress.new
