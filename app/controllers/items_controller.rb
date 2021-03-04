b28919e122bb9307d757b9436cf3162b87714ad2
absurpone <absurppone@gmail.com>
2021-03-04 11:20:03 +0900

    items_controllerの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, except: [:index, :new, :create]
   before_action :contributor_confirmation, only: [:edit, :update, :destroy]
+  before_action :check_item_sold, only: [:edit, :update]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -24,9 +25,6 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    if @item.purchase.present?
-      redirect_to root_path
-    end
   end
 
   def update
@@ -58,4 +56,10 @@ end
 
 def contributor_confirmation
   redirect_to root_path unless current_user == @item.user
+end
+
+def check_item_sold
+  if @item.purchase.present?
+    redirect_to root_path
+  end
 end
